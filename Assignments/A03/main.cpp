f53ee4b824b158986615fd7e58761e65261980da
vizemo <vzerim14games+github@gmail.com>
2024-09-07 09:40:11 -0500

    forgot to upload yesterday

---
+++ Assignments/A03/main.cpp
@@ -0,0 +1,22 @@
+/**
+* Name: Victor Marchesi
+* Course: CMPS 4883
+* Date: 9/6/2024
+*/
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+    long B = 0, A = 0;
+
+    while (cin >> A >> B) {
+
+        // Calculate the absolute difference
+        cout << abs(B - A) << endl;
+
+    }
+    return 0;
+}
